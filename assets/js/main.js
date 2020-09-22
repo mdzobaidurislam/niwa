3df13b536eeaef1b901e402c82fe1b25b0749b06
freelancerjami <freelancerjami@gmail.com>
2020-09-22 19:50:53 +0600

    first-commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+(function ($) {
+    "use strict";
+
+    $('#show_mobaile_menu i').click(function () {
+        $('#mobail_menu_hide').addClass('show_mobaile');
+    });
+
+    $('#hide_menu').click(function () {
+        $('#mobail_menu_hide').removeClass('show_mobaile');
+    });
+
+
+})(jQuery);
