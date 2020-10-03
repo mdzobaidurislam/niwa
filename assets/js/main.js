c171d0350e11fad277d929e3d85fab87488be6ff
freelancerjami <freelancerjami@gmail.com>
2020-10-04 00:15:53 +0600

    new code added

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,5 +9,18 @@
         $('#mobail_menu_hide').removeClass('show_mobaile');
     });
 
+    $('.tigger-m').click(function () {
+        $('.mobaile-menu').toggleClass('m-open');
+        $('.tigger-m').toggleClass('tigger-m-c');
+    });
+    $('#close').click(function () {
+        $('.mobaile-menu').removeClass('m-open');
+        $('.tigger-m').removeClass('tigger-m-c');
+    });
+    $('#close1').click(function () {
+        $('.mobaile-menu').removeClass('m-open');
+        $('.tigger-m').show();
+        $('#close1').hide();
+    });
 
 })(jQuery);
